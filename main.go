c9d151e5a65a396a7e8872ed7798ba36ad5574ad
Samuel Nelson <valczir.darkvein@gmail.com>
2015-12-05 11:41:33 -0700

    Fixed up main for current generation logic

--- main.go
+++ main.go
@@ -3,7 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"time"
 
+	"github.com/nelsam/hel/mocks"
+	"github.com/nelsam/hel/packages"
+	"github.com/nelsam/hel/types"
 	"github.com/spf13/cobra"
 )
 
@@ -25,28 +30,92 @@ func init() {
 				}
 				os.Exit(1)
 			}
-			packages, err := cmd.Flags().GetStringSlice("package")
+			packagePatterns, err := cmd.Flags().GetStringSlice("package")
 			if err != nil {
 				panic(err)
 			}
-			types, err := cmd.Flags().GetStringSlice("type")
+			typePatterns, err := cmd.Flags().GetStringSlice("type")
 			if err != nil {
 				panic(err)
 			}
-			output, err := cmd.Flags().GetString("output")
+			outputName, err := cmd.Flags().GetString("output")
 			if err != nil {
 				panic(err)
 			}
-			fmt.Printf("Generating mocks for packages %v", packages)
-			fmt.Printf("Generating mocks for types %v", types)
-			fmt.Printf("Generating mocks in %s", output)
+			chanSize, err := cmd.Flags().GetInt("chan-size")
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("Generating mocks for packages %v", packagePatterns)
+			var packageList []packages.Package
+			progress(func() {
+				packageList = packages.Load(packagePatterns...)
+			})
+			fmt.Print("\n")
+			fmt.Printf("Generating mocks for types %v", typePatterns)
+			var typeList []types.Types
+			progress(func() {
+				typeList = types.Load(packageList...) //.Filter(typePatterns...)
+			})
+			fmt.Print("\n")
+			fmt.Printf("Generating mocks in %s", outputName)
+			progress(func() {
+				for _, types := range typeList {
+					if err := makeMocks(types, outputName, chanSize); err != nil {
+						panic(err)
+					}
+				}
+			})
+			fmt.Print("\n")
 		},
 	}
 	cmd.Flags().StringSliceP("package", "p", []string{"."}, "The package(s) to generate mocks for.")
 	cmd.Flags().StringSliceP("type", "t", []string{}, "The type(s) to generate mocks for.  If no types "+
- "are passed in, all exported interface types will be generated.")
-	cmd.Flags().StringP("output", "o", "hel.go", "The file to write generated mocks to.  Since hel does "+
- "not generate exported types, this file will be saved directly in all packages with generated mocks.")
+		"are passed in, all exported interface types will be generated.")
+	cmd.Flags().StringP("output", "o", "hel_test.go", "The file to write generated mocks to.  Since hel does "+
+		"not generate exported types, this file will be saved directly in all packages with generated mocks.  "+
+		"Also note that, since the types are not exported, you will want the file to end in '_test.go'.")
+	cmd.Flags().IntP("chan-size", "s", 100, "The size of channels used for method calls.")
+}
+
+func makeMocks(types types.Types, fileName string, chanSize int) error {
+	mocks, err := mocks.Generate(types)
+	if err != nil {
+		return err
+	}
+	if len(mocks) == 0 {
+		return nil
+	}
+	f, err := os.Create(filepath.Join(types.Dir(), fileName))
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return mocks.Output(types.TestPackage(), chanSize, f)
+}
+
+func progress(f func()) {
+	stop, done := make(chan struct{}), make(chan struct{})
+	defer func() {
+		close(stop)
+		<-done
+	}()
+	go showProgress(stop, done)
+	f()
+}
+
+func showProgress(stop <-chan struct{}, done chan<- struct{}) {
+	defer close(done)
+	ticker := time.NewTicker(time.Second / 2)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ticker.C:
+			fmt.Print(".")
+		case <-stop:
+			return
+		}
+	}
 }
 
 func main() {
