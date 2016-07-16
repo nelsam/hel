3217eac12b292dee4e41356ddb8f131b410fb259
Samuel Nelson <valczir.darkvein@gmail.com>
2016-07-16 16:53:11 -0600

    Add license blurb to all .go file headers

--- types/doc.go
+++ types/doc.go
@@ -1,3 +1,7 @@
+// This is free and unencumbered software released into the public
+// domain.  For more information, see <http://unlicense.org> or the
+// accompanying UNLICENSE file.
+
 // Package types contains logic for parsing type definitions from ast
 // packages and filtering those types.
 package types
