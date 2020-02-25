1eadb2bf36f8cb5bc85e9bebce19ea37d8938e2b
Samuel Nelson <valczir.darkvein@gmail.com>
2020-02-25 00:07:37 -0600

    pers: use onpar's new matchers.Differ type to generate a diff between values

--- pers/havebeenexecuted.go
+++ pers/havebeenexecuted.go
@@ -7,8 +7,10 @@ package pers
 import (
 	"fmt"
 	"reflect"
-	"strings"
 	"time"
+
+	"github.com/poy/onpar/diff"
+	"github.com/poy/onpar/matchers"
 )
 
 // HaveMethodExecutedOption is an option function for the HaveMethodExecutedMatcher.
@@ -39,16 +41,22 @@ type HaveMethodExecutedMatcher struct {
 	MethodName string
 	within     time.Duration
 	args       []interface{}
+	differ     matchers.Differ
 }
 
 // HaveMethodExecuted returns a matcher that asserts that the method referenced
 // by name was executed.  Options can modify the behavior of the matcher.
-func HaveMethodExecuted(name string, opts ...HaveMethodExecutedOption) HaveMethodExecutedMatcher {
-	m := HaveMethodExecutedMatcher{MethodName: name}
+func HaveMethodExecuted(name string, opts ...HaveMethodExecutedOption) *HaveMethodExecutedMatcher {
+	m := HaveMethodExecutedMatcher{MethodName: name, differ: diff.New()}
 	for _, opt := range opts {
 		m = opt(m)
 	}
-	return m
+	return &m
+}
+
+// UseDiffer sets m to use d when showing a diff between actual and expected values.
+func (m *HaveMethodExecutedMatcher) UseDiffer(d matchers.Differ) {
+	m.differ = d
 }
 
 // Match checks the mock value v to see if it has a method matching m.MethodName
@@ -105,147 +113,13 @@ func (m HaveMethodExecutedMatcher) Match(v interface{}) (interface{}, error) {
 	}
 
 	matched := true
-	var actual, expected []string
 	for i, a := range args {
 		called := calledWith[i]
-		match, a, e := diff(called, a)
-		actual = append(actual, a)
-		expected = append(expected, e)
-		matched = matched && match
+		matched = matched && reflect.DeepEqual(called, a)
 	}
 	if matched {
 		return v, nil
 	}
-	msg := "pers: %s was called with (%s); expected (%s)"
-	return v, fmt.Errorf(msg, m.MethodName, strings.Join(actual, ", "), strings.Join(expected, ", "))
-}
-
-func diff(actual, expected interface{}) (matched bool, actualOutput, expectedOutput string) {
-	return diffV(reflect.ValueOf(actual), reflect.ValueOf(expected))
-}
-
-type span struct {
-	start, end int
-}
-
-func diffV(av, ev reflect.Value) (matched bool, actualOutput, expectedOutput string) {
-	if av.Kind() != ev.Kind() {
-		format := ">type mismatch: %#v<"
-		return false, fmt.Sprintf(format, av.Interface()), fmt.Sprintf(format, ev.Interface())
-	}
-	if av.Type().Comparable() {
-		matched := true
-		format := "%#v"
-		if av.Interface() != ev.Interface() {
-			matched = false
-			format = ">%#v<"
-		}
-		return matched, fmt.Sprintf(format, av.Interface()), fmt.Sprintf(format, ev.Interface())
-	}
-
-	switch av.Interface().(type) {
-	case []rune, []byte:
-		// make almost-string types a little prettier, when possible.
-		if av.Len() != ev.Len() {
-			break // let the default logic handle this
-		}
-
-		strTyp := reflect.TypeOf("")
-		matchSection := true
-		matched := true
-		var outa, oute string
-		for i := 0; i < av.Len(); i++ {
-			match, _, _ := diffV(av.Index(i), ev.Index(i))
-			if !match && matchSection {
-				outa += ">"
-				oute += ">"
-			}
-			if match && !matchSection {
-				outa += "<"
-				oute += "<"
-			}
-			matchSection = match
-			matched = matched && match
-			outa += av.Index(i).Convert(strTyp).Interface().(string)
-			oute += ev.Index(i).Convert(strTyp).Interface().(string)
-		}
-		return matched, outa, oute
-	}
-
-	switch av.Kind() {
-	case reflect.Ptr, reflect.Interface:
-		return diffV(av.Elem(), ev.Elem())
-	case reflect.Slice, reflect.Array:
-		if av.Len() != ev.Len() {
-			// TODO: do a more thorough diff of values
-			format := ">%T(length %d)<"
-			return false, fmt.Sprintf(format, av.Interface(), av.Len()), fmt.Sprintf(format, ev.Interface(), ev.Len())
-		}
-		format := func(parts []string) string {
-			return "[" + strings.Join(parts, ",") + "]"
-		}
-		var aParts, eParts []string
-		matched := true
-		for i := 0; i < av.Len(); i++ {
-			match, a, e := diffV(av.Index(i), ev.Index(i))
-			matched = matched && match
-			aParts = append(aParts, a)
-			eParts = append(eParts, e)
-		}
-		return matched, format(aParts), format(eParts)
-	case reflect.Map:
-		format := func(parts []string) string {
-			return "{" + strings.Join(parts, ",") + "}"
-		}
-		var aParts, eParts []string
-		matched := true
-		for _, kv := range ev.MapKeys() {
-			k := kv.Interface()
-			emv := ev.MapIndex(kv)
-			amv := av.MapIndex(kv)
-			if !amv.IsValid() {
-				aParts = append(aParts, fmt.Sprintf(">missing key %v<", k))
-				eParts = append(eParts, fmt.Sprintf(">%v: %v<", k, emv.Interface()))
-				continue
-			}
-			match, a, e := diffV(amv, emv)
-			matched = matched && match
-			aParts = append(aParts, fmt.Sprintf("%v: %s", k, a))
-			eParts = append(eParts, fmt.Sprintf("%v: %s", k, e))
-		}
-		for _, kv := range av.MapKeys() {
-			// We've already compared all keys that exist in both maps; now we're
-			// just looking for keys that only exist in the actual.
-			k := kv.Interface()
-			if !ev.MapIndex(kv).IsValid() {
-				matched = false
-				aParts = append(aParts, fmt.Sprintf(">extra key %v: %v<", k, av.MapIndex(kv).Interface()))
-				eParts = append(eParts, fmt.Sprintf(">%v: nil<", k))
-				continue
-			}
-		}
-		return matched, format(aParts), format(eParts)
-	case reflect.Struct:
-		if av.Type().Name() != ev.Type().Name() {
-			return false, ">" + av.Type().Name() + "(mismatched types)<", ">" + ev.Type().Name() + "(mismatched types)<"
-		}
-		format := func(parts []string) string {
-			return fmt.Sprintf("%T{\n", av.Interface()) + strings.Join(parts, ",\n") + "}"
-		}
-		var aParts, eParts []string
-		matched := true
-		for i := 0; i < ev.NumField(); i++ {
-			name := ev.Type().Field(i).Name
-			efv := ev.Field(i)
-			afv := av.Field(i)
-			match, a, e := diffV(afv, efv)
-			matched = matched && match
-			aParts = append(aParts, fmt.Sprintf("%s: %s", name, a))
-			eParts = append(eParts, fmt.Sprintf("%s: %s", name, e))
-		}
-		return matched, format(aParts), format(eParts)
-	default:
-		msg := fmt.Sprintf("> UNSUPPORTED: could not compare values of type %T <", av.Interface())
-		return false, msg, msg
-	}
+	msg := "pers: %s was called with incorrect arguments: %s"
+	return v, fmt.Errorf(msg, m.MethodName, m.differ.Diff(calledWith, args))
 }
